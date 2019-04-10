4c3825406cbbfcfa1ccef4d29e6c4afd7c9f0a39
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2019-04-09 22:37:52 -0400

    Add select ingredients paragraph when burger has no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,11 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+  const buildMessage = (
+    <p className={classes['build-message']}>
+      Select your ingredients from the list
+    </p>);
+
   const ingredientClasses = ['bread-top'];
 
   for (let ing in props.ingredients) {
@@ -15,6 +20,11 @@ const burger = (props) => {
 
   const burgerIngredients = ingredientClasses.map(
     (cssClass, index) => <BurgerIngredient key={index} type={cssClass} />);
+
+  if (burgerIngredients.length === 2) {
+    burgerIngredients.splice(1, 0, buildMessage);
+    console.log(burgerIngredients);
+  }
 
   return (
     <div className={classes['burger']}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,11 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      lettuce: 1,
-      tomato: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      lettuce: 0,
+      tomato: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
 
